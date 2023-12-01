b15aef0ac3f831527d6d04ed599dc936ac0728ce
RIG360 <office@rimpfl.at>
2023-12-01 14:47:00 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,513 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vorraum",
+      "name": "Vorraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.7720910691124345,
+        "pitch": 0.026926781359209784,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10329374971009031,
+          "pitch": 0.005068251528385304,
+          "rotation": 0,
+          "target": "4-gang"
+        },
+        {
+          "yaw": 0.5297121186265947,
+          "pitch": 0.016968656931801718,
+          "rotation": 0,
+          "target": "3-bad"
+        },
+        {
+          "yaw": 1.297861848975245,
+          "pitch": 0.021463593676370962,
+          "rotation": 0,
+          "target": "2-wc"
+        },
+        {
+          "yaw": 2.1242804337931265,
+          "pitch": 0.017683097654625612,
+          "rotation": 0,
+          "target": "1-abstellraum"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-abstellraum",
+      "name": "Abstellraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -1.262115126217676,
+        "pitch": 0.006474756749526023,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.262115126217676,
+          "pitch": 0.006474756749526023,
+          "rotation": 0,
+          "target": "0-vorraum"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-wc",
+      "name": "WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.5911691657118361,
+        "pitch": 0.3229677046715622,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4023258761675113,
+          "pitch": 0.003381576682601306,
+          "rotation": 0,
+          "target": "0-vorraum"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bad",
+      "name": "Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.7539822368615638,
+        "pitch": 0.0219618039176801,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5236992571561565,
+          "pitch": 0.016760861782788794,
+          "rotation": 0,
+          "target": "0-vorraum"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-gang",
+      "name": "Gang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.4742948386377286,
+        "pitch": 0.008700957115570063,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1133168332038927,
+          "pitch": 0.0409955420787913,
+          "rotation": 0,
+          "target": "0-vorraum"
+        },
+        {
+          "yaw": 1.2538278009529336,
+          "pitch": 0.07023275892532155,
+          "rotation": 0,
+          "target": "5-zimmer-1"
+        },
+        {
+          "yaw": -0.284521844828479,
+          "pitch": -0.005371248249563365,
+          "rotation": 0,
+          "target": "6-zimmer-2"
+        },
+        {
+          "yaw": -2.2019229370769384,
+          "pitch": 0.019228716560839132,
+          "rotation": 0,
+          "target": "7-kochbereich"
+        },
+        {
+          "yaw": -1.4670972254680912,
+          "pitch": 0.02039454801392182,
+          "rotation": 0,
+          "target": "8-essbereich"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-zimmer-1",
+      "name": "Zimmer 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.146656768061476,
+          "pitch": -0.001063893615386391,
+          "rotation": 0,
+          "target": "4-gang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-zimmer-2",
+      "name": "Zimmer 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.42078301602626667,
+        "pitch": -0.019378062280289043,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.086946596245981,
+          "pitch": -0.046595273921919755,
+          "rotation": 0,
+          "target": "4-gang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-kochbereich",
+      "name": "Kochbereich",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -2.600734510943999,
+        "pitch": 0.03229772197916958,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6704555078743528,
+          "pitch": 0.011794311677196134,
+          "rotation": 0,
+          "target": "9-wohnbereich"
+        },
+        {
+          "yaw": -1.5499743753071407,
+          "pitch": 0.013353224596080793,
+          "rotation": 0,
+          "target": "8-essbereich"
+        },
+        {
+          "yaw": 1.608834051507908,
+          "pitch": 0.08910509761856389,
+          "rotation": 0,
+          "target": "4-gang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-essbereich",
+      "name": "Essbereich",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.2922633165612396,
+        "pitch": -0.001291870818683094,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7833999587377285,
+          "pitch": 0.002103973114927271,
+          "rotation": 0,
+          "target": "9-wohnbereich"
+        },
+        {
+          "yaw": 2.0631723392776724,
+          "pitch": 0.015163820059791888,
+          "rotation": 0,
+          "target": "7-kochbereich"
+        },
+        {
+          "yaw": 1.6350645212231258,
+          "pitch": 0.024426585935970024,
+          "rotation": 0,
+          "target": "4-gang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-wohnbereich",
+      "name": "Wohnbereich",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.3084472787160859,
+        "pitch": -0.0038756124560617167,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7306744133832055,
+          "pitch": 0.0053574474640907255,
+          "rotation": 0,
+          "target": "8-essbereich"
+        },
+        {
+          "yaw": 0.13029809777416368,
+          "pitch": -0.01206114453176177,
+          "rotation": 0,
+          "target": "10-balkon"
+        },
+        {
+          "yaw": 2.7592986023968704,
+          "pitch": 0.012437094852113972,
+          "rotation": 0,
+          "target": "7-kochbereich"
+        },
+        {
+          "yaw": 2.1419919378947823,
+          "pitch": 0.010427569941967363,
+          "rotation": 0,
+          "target": "4-gang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-balkon",
+      "name": "Balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.8977339007076424,
+        "pitch": 0.01937806228029615,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0936423521275174,
+          "pitch": 0.03501649850649535,
+          "rotation": 0,
+          "target": "9-wohnbereich"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
